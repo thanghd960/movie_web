bd2cc2fdfe262bf83b4b681d97e9c12a53f1e2d7
DaiThang <huadaithang@gamil.com>
2017-07-18 17:31:11 +0700

    add rating

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,6 +11,12 @@ class MoviesController < ApplicationController
   # GET /movies/1
   # GET /movies/1.json
   def show
+    @review = Review.where(movie_id: @movie.id).order("created_at DESC")
+    if @review.blank?
+        @avg_review = 0
+    else
+        @avg_review = @review.average(:rating).round(2)
+    end
   end
 
   # GET /movies/new
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170718084058) do
+ActiveRecord::Schema.define(version: 20170718092227) do
 
   create_table "divises", force: :cascade do |t|
     t.string "User"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20170718084058) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "movie_id"
   end
 
   create_table "users", force: :cascade do |t|
